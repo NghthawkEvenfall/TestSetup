d9fbb0f1a2cbf8164b8cbb58c39851e85ef133ff
NghthawkEvenfall <nghthawk74@gmail.com>
2016-11-03 12:20:39 -0400

    Inital Deployment

---
+++ JenkinsTestApp/Controllers/ExpensesController.vb
@@ -0,0 +1,32 @@
+﻿Imports System.Net
+Imports System.Net.Http
+Imports System.Web.Http
+Imports Newtonsoft.Json
+Imports System.Data.SqlClient
+Imports System.Globalization
+Imports JenkinsTestApp.TheatreServices.Models.TheatreServices
+
+Namespace Controllers
+    Public Class ExpensesController
+        Inherits ApiController
+
+        <Route("api/v1/jobs/report/{jobID}")>
+        Public Function GetReport(<FromUri> value As GetJobID) As Object
+
+
+
+            Dim List
+
+
+
+            List = New With {
+                .data = Nothing,
+                .message = "Testing"
+            }
+
+
+
+            Return List
+        End Function
+    End Class
+End Namespace
---
+++ JenkinsTestApp/Models/TheatreServices.vb
@@ -0,0 +1,12 @@
+﻿Imports System.ComponentModel.DataAnnotations
+
+Namespace TheatreServices.Models
+    Public Class TheatreServices
+
+        Public Class GetJobID
+            Public Property jobID As String
+        End Class
+
+    End Class
+
+End Namespace
